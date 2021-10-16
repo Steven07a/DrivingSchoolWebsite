8c18cccb97ea56180d8d4f99e27241c39ef7ecc3
Felix Zhang <felix.zhang@lacity.org>
2021-10-15 22:55:10 -0700

    added basic routing from login to homepage. Added conditional navbar for login page

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,40 +1,73 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useLocation } from "react-router-dom";
 import { menuItems } from "./NavBarItems";
-import { Button } from "../Button/Button";
 import "../../styles/NavBar.css";
 
 
-class NavBar extends Component {
-    state = {
-        clicked: false
-    }
+export default function NavBar() {
+    const location = useLocation();
 
 
-    render() {
-        return (
-            <nav className="NavBarItems">
-                <h1 className="navbar-logo">
-                    <i class="fas fa-car-side"></i>
-                    South Pass Student Dashboard
-                    <i class="fas fa-car-side"></i>
-                </h1>
-                <div className="menu-icon" onClick={this.handleClick}>
-                </div>
-                <ul className="nav-menu">
-                    {menuItems.map((item, index) => {
-                        return (
-                            <li key={index}>
-                                <a className={item.className} href={item.url}>
-                                    {item.title}
-                                </a>
-                            </li>
-                        );
-                    })}
-                </ul>
-                <Button> Sign In</Button>
-            </nav>
-        )
+    if (location.pathname.match("/login")) {
+        return null;
     }
+
+    return (
+        <nav className="NavBarItems">
+            <h1 className="navbar-logo">
+                <i class="fas fa-car-side"></i>
+                South Pass Student Dashboard
+                <i class="fas fa-car-side"></i>
+            </h1>
+            <ul className="nav-menu">
+                {menuItems.map((item, index) => {
+                    return (
+                        <li key={index}>
+                            <a className={item.className} href={item.url}>
+                                {item.title}
+                            </a>
+                        </li>
+                    );
+                })}
+            </ul>
+        </nav>
+    );
 }
 
-export default NavBar;
+// class NavBar extends Component {
+//     state = {
+//         clicked: false
+//     }
+
+
+
+//     render() {
+//         console.log(useHistory());
+
+
+//         return (
+//             <nav className="NavBarItems">
+//                 <h1 className="navbar-logo">
+//                     <i class="fas fa-car-side"></i>
+//                     South Pass Student Dashboard
+//                     <i class="fas fa-car-side"></i>
+//                 </h1>
+//                 <div className="menu-icon" onClick={this.handleClick}>
+//                 </div>
+//                 <ul className="nav-menu">
+//                     {menuItems.map((item, index) => {
+//                         return (
+//                             <li key={index}>
+//                                 <a className={item.className} href={item.url}>
+//                                     {item.title}
+//                                 </a>
+//                             </li>
+//                         );
+//                     })}
+//                 </ul>
+//             </nav>
+//         )
+//     }
+// }
+
+// export default NavBar;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import { useHistory } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { Button, TextField } from "@mui/material";
 
@@ -7,13 +8,21 @@ import "../styles/login.css";
 
 export default function Login() {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const history = useHistory();
+
+    /*
+    testing the routing here, need to handle login info 
+    submission
+    */
+    const onSubmit = () => {
+        history.push("/")
+    };
 
 
 
     return (
         <div className="login">
-            <h1 className="loginText">Login</h1>
+            <h1 className="loginText">Welcome</h1>
             <form className="formContainer" onSubmit={handleSubmit(onSubmit)}>
                 <div className="formItem">
                     <label>Username</label>
@@ -22,6 +31,13 @@ export default function Login() {
                 <div className="formItem">
                     <label>Password</label>
                     <TextField {...register("password")} />
+                </div>
+                {/*change this to a LoadingButton later */}
+                {/* #TODO match size of button with input box above */}
+                <div className="submitBtn">
+                    <Button 
+                    variant="contained"
+                    type="submit">Log In</Button>
                 </div>
             </form>
         </div>
