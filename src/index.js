9cb541d1ec01efbecec481518e3b83d156f8b5f6
Felix Zhang <felix.zhang@lacity.org>
2021-10-21 20:25:47 -0700

    setting up aws amplify for auth

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import  ReactDOM  from "react-dom";
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+
+
 import './styles/app.css';
 import  App from "./App";
 
+
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <App />,
