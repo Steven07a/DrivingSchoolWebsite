b2b643fa7aaaf478bccd13cd2b6a768aab4a21a9
steven07a <stevenherrera70396@gmail.com>
2022-02-21 13:08:31 -0800

    navbar with hamburger menu for mobile is working

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from "react-router-dom";
 import { menuItems } from "./NavBarItems";
 import "../../styles/NavBar.css";
+import { Button } from "./Button"
+
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
 
 export default function NavBar() {
     const location = useLocation();
 
+    const [clicked, setClicked] = useState(0);
 
     if (location.pathname.match("/login")) {
         return null;
     }
+
+    //example of functional state
+    // const [student,setStudent] = useState(
+    //     {
+    //         studentName: "",
+    //         studentId: "",
+    //         studentAdd: ""
+
+    //     }
+    // )
 
     return (
         <nav className="NavBarItems">
@@ -20,22 +33,22 @@ export default function NavBar() {
                 South Pass Student Dashboard
                 <i class="fas fa-car-side"></i>
             </h1>
-            <div class="burger">
-                <div class="line1"></div>
-                <div class="line2"></div>
-                <div class="line3"></div>
-            </div>
-            <ul className="nav-menu">
-                {menuItems.map((item, index) => {
-                    return (
-                        <li key={index}>
-                            <a className={item.className} href={item.url}>
-                                {item.title}
-                            </a>
-                        </li>
-                    );
-                })}
+                <div className="menu-icon" onClick={() => setClicked(!clicked)}>
+                    <i className={clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
+                    {menuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.className} href={item.url}>
+                                    {item.title}
+                                </a>
+                            </li>
+                        );
+                    })}
             </ul>
+            
+            <Button>Sign Up</Button>
         </nav>
         // <nav className="NavBarItems">
         //     <h1 className="navbar-logo">
@@ -57,41 +70,3 @@ export default function NavBar() {
         // </nav>
     );
 }
-
-// class NavBar extends Component {
-//     state = {
-//         clicked: false
-//     }
-
-
-
-//     render() {
-//         console.log(useHistory());
-
-
-//         return (
-//             <nav className="NavBarItems">
-//                 <h1 className="navbar-logo">
-//                     <i class="fas fa-car-side"></i>
-//                     South Pass Student Dashboard
-//                     <i class="fas fa-car-side"></i>
-//                 </h1>
-//                 <div className="menu-icon" onClick={this.handleClick}>
-//                 </div>
-//                 <ul className="nav-menu">
-//                     {menuItems.map((item, index) => {
-//                         return (
-//                             <li key={index}>
-//                                 <a className={item.className} href={item.url}>
-//                                     {item.title}
-//                                 </a>
-//                             </li>
-//                         );
-//                     })}
-//                 </ul>
-//             </nav>
-//         )
-//     }
-// }
-
-// export default NavBar;
